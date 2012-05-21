a41796940c92247b4448f78e4627e42d6017f3a3
Fabio Vilela <fbvilela@gmail.com>
2012-05-22 01:22:33 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wedd::Application.config.secret_token = 'e41f70da481a39b3532ada577fc2f3c4ed10aa662891c3e0ad0e9c58c3e0892876ea968611305c56296ed3a0ad40d16d5e4c75086a42543adf7513018eba5f00'
